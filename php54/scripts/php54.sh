44818eb91faabe27bfd509b0a9e56e4e68313a4f
Betobaz <pedroalberto007@gmail.com>
2016-08-10 16:02:07 -0500

    Add node and composer support

--- php54/scripts/php54.sh
+++ php54/scripts/php54.sh
@@ -52,3 +52,18 @@ sed -i 's/;date.timezone =/date.timezone = UTC/' /etc/php5/cli/php.ini
 # Adjust APC settings to more appropriate values for Sugar
 echo "apc.shm_size=378M" >> /etc/php5/mods-available/apc.ini
 echo "apc.max_file_size=10M" >> /etc/php5/mods-available/apc.ini
+
+# Install node.js support
+wget -qO- https://deb.nodesource.com/setup_4.x | bash -
+apt-get install nodejs
+
+# Install grunt-cli support
+npm install -g grunt-cli
+
+# Install composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === 'e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
+mv composer.phar /usr/local/bin/composer
