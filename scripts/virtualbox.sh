ddc88fcdc1accb7154401676aa7eccca4e4ef4a1
Matt Marum <mmarum@sugarcrm.com>
2017-09-07 15:16:53 -0400

    Add vbox guest additions

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -6,9 +6,11 @@ if [[ `dmidecode -s system-product-name` != "VirtualBox" ]]; then
 fi
 
 
-mkdir -p /mnt/virtualbox
-mount -o loop $HOME/VBoxGuest*.iso /mnt/virtualbox
-sh /mnt/virtualbox/VBoxLinuxAdditions.run
-ln -s /opt/VBoxGuestAdditions-*/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
-umount /mnt/virtualbox
-rm -rf $HOME/VBoxGuest*.iso
+# mkdir -p /mnt/virtualbox
+# mount -o loop $HOME/VBoxGuest*.iso /mnt/virtualbox
+# sh /mnt/virtualbox/VBoxLinuxAdditions.run
+# ln -s /opt/VBoxGuestAdditions-*/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
+# umount /mnt/virtualbox
+# rm -rf $HOME/VBoxGuest*.iso
+
+apt-get -y install virtualbox-guest-dkms
